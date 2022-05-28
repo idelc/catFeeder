317c4f032a9a7345e4d5958433705ef878638acf
idelc <idel012@ucr.edu>
2022-05-28 03:46:43 -0700

    Used timer library made specifically for the esp chip, and tested it with onboard led, finished all wiring and testing

--- project2.ino
+++ project2.ino
@@ -1,37 +1,88 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include "ESP8266TimerInterrupt.h"
+#include "ESP8266_ISR_Timer.hpp"   
 
-#define RST_PIN 2
-#define SS_PIN  15
+#define USING_TIM_DIV1                false           // for shortest and most accurate timer
+#define USING_TIM_DIV16               false           // for medium time and medium accurate timer
+#define USING_TIM_DIV256              true            // for longest timer but least accurate. Default
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+// Init ESP8266 only and only Timer 1
+ESP8266Timer ITimer;
 
-void setup(){
-    Serial.begin(9600);
-    while(!Serial);
-    SPI.begin();
-    mfrc522.PCD_Init();
-    delay(4);
-    mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
-    mfrc522.PCD_DumpVersionToSerial();
-    Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+// interval (in microseconds)
+bool setInterval(unsigned long interval, timer_callback callback);
+
+// interval (in microseconds)
+bool attachInterruptInterval(unsigned long interval, timer_callback callback);
+
+bool led = true;
+void IRAM_ATTR TimerHandler()
+{
+  // Doing something here inside ISR
+  if(led){
+      digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else{
+      digitalWrite(LED_BUILTIN, LOW);
+  }
+  led = !led;
 }
 
-int gain = 0;
+#define TIMER_INTERVAL_MS        1000
+
+unsigned long lastMillis = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Interval in microsecs
+  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler))
+  {
+    lastMillis = millis();
+    Serial.print(F("Starting  ITimer OK, millis() = ")); Serial.println(lastMillis);
+  }
+  else
+    Serial.println(F("Can't set ITimer correctly. Select another freq. or interval"));
+}  
+
 void loop(){
-    if(!mfrc522.PICC_IsNewCardPresent()){
-        return;
-    }
-    if(!mfrc522.PICC_ReadCardSerial()){
-        return;
-    }
-    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-    gain = mfrc522.PCD_GetAntennaGain();
-    Serial.print("Antena Gain: ");
-    Serial.println(gain);
-    delay(1000);
+
 }
 
+// #include <SPI.h>
+// #include <MFRC522.h>
+
+// #define RST_PIN 2
+// #define SS_PIN  15
+
+// MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// void setup(){
+//     Serial.begin(9600);
+//     while(!Serial);
+//     SPI.begin();
+//     mfrc522.PCD_Init();
+//     delay(4);
+//     mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+//     mfrc522.PCD_DumpVersionToSerial();
+//     Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+// }
+
+// int gain = 0;
+// void loop(){
+//     if(!mfrc522.PICC_IsNewCardPresent()){
+//         return;
+//     }
+//     if(!mfrc522.PICC_ReadCardSerial()){
+//         return;
+//     }
+//     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+//     gain = mfrc522.PCD_GetAntennaGain();
+//     Serial.print("Antena Gain: ");
+//     Serial.println(gain);
+//     delay(1000);
+// }
 
 
 // #include <Stepper.h>
