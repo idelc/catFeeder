23006f33d0bbaa0d87ae9caf74d7b641991f4c97
idelc <idel012@ucr.edu>
2022-05-29 00:59:13 -0700

    General Formating and Motion Sensor State Machine implementation

--- project2.ino
+++ project2.ino
@@ -6,6 +6,8 @@
 
 #include <Stepper.h>                // Stepper library
 
+//==========================
+
 // Timer definitions (Prescalers)
 #define USING_TIM_DIV1                false           // for shortest and most accurate timer
 #define USING_TIM_DIV16               false           // for medium time and medium accurate timer
@@ -18,6 +20,10 @@
 // Stepper definitions
 #define stepsPerRev 64
 
+// Motion Sensor definitions
+#define moSens -1
+
+//==========================
 
 // Init ESP8266 only and only Timer 1
 ESP8266Timer ITimer; 
@@ -26,36 +32,113 @@ bool setInterval(unsigned long interval, timer_callback callback);
 // Timer interval (in microseconds)
 bool attachInterruptInterval(unsigned long interval, timer_callback callback);
 
-void IRAM_ATTR TimerHandler()
-{
-  // Doing something here inside ISR
-}
-
 // Init RFID
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
 // Init Stepper Motor
 Stepper lidMotor(stepsPerRev, 16, 5, 4, 0);
 
-void setup()
-{ 
-// Timer Init
+// scheduler
+typedef struct task{
+  int state;
+  unsigned long period;
+  unsigned long elapsedTime;
+  int (*TickFct) (int);
+} task;
+
+task taskArray[5];
+
+//==========================
+
+// Variables
+// Scheduler variables
+const unsigned char tasksNum = 4;
+const unsigned long tasksPeriodGDC = 50;
+
+// Motion Sensor variables
+volatile short motionFlag = 0;
+volatile int motionCntr = 0;
+
+//==========================
+
+void IRAM_ATTR TimerHandler(){
+  unsigned char i;
+  for(i = 0; i < tasksNum; ++i){
+    if(taskArray[i].elapsedTime >= taskArray[i].period){
+      taskArray[i].state = taskArray[i].TickFct(taskArray[i].state);
+      taskArray[i].elapsedTime = 0;
+    }
+    taskArray[i].elapsedTime += tasksPeriodGDC;
+  }
+}
+
+//==========================
+// Tick Function Declarations 
+enum MoS_States{Mos_Start, Mos_Wait, Mos_Detd, Mos_Hold};
+int TickFct_MoSensor(int state);
+
+void setup(){ 
+  // Timer Init
   // Set Interval in microsecs
-  /*
-  if (!ITimer.attachInterruptInterval(timerGCD * 1000, TimerHandler))
+  if (!ITimer.attachInterruptInterval(tasksPeriodGDC * 1000, TimerHandler))
   {
     exit(1);
   }
-  */
-// RFID Init
-  // SPI.begin();
-  // mfrc522.PCD_Init();
-  // delay(4);
-  // mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max); // max read distance
-// Stepper Motor Init
-// lidMotor.setSpeed(30); // arbitrary speed, 30 rpm
+  // RFID Init
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(4);
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max); // max read distance
+  // Stepper Motor Init
+  lidMotor.setSpeed(30); // arbitrary speed, 30 rpm
+  // Scheduler Setup // TODO: Finish once the tasks are done
+  // unsigned i = 0;
+  // taskArray[i].state = ST_Start;
+  // taskArray[i].period = periodLCD;
+  // taskArray[i].elapsedTime = periodLCD;
+  // taskArray[i].TickFct = &lcdScreenSet;
+  // i++;
 }  
 
 void loop(){
+  // Empty, code will only be executed on interrupt :D
+}
 
+int TickFct_MoSensor(int state){
+  switch(state){
+    case Mos_Start:
+      state = Mos_Wait;
+      break;
+    case Mos_Wait:
+      state = digitalRead(moSens) ? Mos_Detd : Mos_Wait;
+      break;
+    case Mos_Detd:
+      state = Mos_Hold;
+      break;
+    case Mos_Hold:
+      state = (motionCntr < 30) ? Mos_Wait : Mos_Hold;  // Arbitrarily set to 30. TODO: Set to meaningful number
+      break;
+    default:
+      state = Mos_Start;
+      break;
+  }
+  switch(state){
+    case Mos_Start:
+      break;
+    case Mos_Wait:
+      motionFlag = 0;
+      break;
+    case Mos_Detd:
+      motionFlag = 1;
+      motionCntr = 0;
+      break;
+    case Mos_Hold:
+      motionCntr++;
+      break;
+    default:
+      motionFlag = 0;
+      motionCntr = 0;
+      break;
+  }
+  return state;
 }
